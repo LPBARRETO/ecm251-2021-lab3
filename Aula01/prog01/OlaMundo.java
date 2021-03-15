70e56e92c0108978c878a1a55b567972ef9f99e5
LPBARRETO <lpbarreto97@gmail.com>
2021-03-15 07:09:33 -0300

    Codando e compilando na mão

--- Aula01/prog01/OlaMundo.java
+++ Aula01/prog01/OlaMundo.java
@@ -1,5 +1,9 @@
 class OlaMundo{
     public static void main(String[] args) {
+        //Isso é um comentário de uma linha
         System.out.println("Ola Mundo!");
+        /* Comentário de diversas linhas
+        *Continuação do comentário
+        */
     }
 }
