1471bca080bb211ad9b82cc5eb7ee42afe1196c2
LPBARRETO <lpbarreto97@gmail.com>
2021-03-15 07:13:51 -0300

    mudança

--- Aula01/prog01/OlaMundo.java
+++ Aula01/prog01/OlaMundo.java
@@ -2,6 +2,7 @@ class OlaMundo{
     public static void main(String[] args) {
         //Isso é um comentário de uma linha
         System.out.println("Ola Mundo!");
+        System.out.println("Stien é mal?");
         /* Comentário de diversas linhas
         *Continuação do comentário
         */
